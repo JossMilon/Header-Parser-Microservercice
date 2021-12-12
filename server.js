f750a8c70676e2fb546f007106c93cd1afb8d1a7
Josselin Milon <josselinmilon@MacBook-Pro-de-Josselin-2.local>
2021-12-12 13:21:42 +0100

    add last modification

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/whoami", (req, res) => {
   res.json({
-    ipaddress: req.headers.ipaddress || "not found",
+    ipaddress: req.ip || "not found",
     language: req.headers["accept-language"] || "not found",
     software: req.headers["user-agent"] || "not found",
   });
